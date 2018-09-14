1c40e89fc263128f7b84cf503aa0b28c9ac0dc6d
Miguel <miguel_reyes_c@hotmail.com>
2018-09-14 00:27:59 -0300

    Building a Structural Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent, BasicHighlightDirective, BetterHighlightDirective
+    AppComponent, BasicHighlightDirective, BetterHighlightDirective, UnlessDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/unless/unless.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+import { ViewContainerData } from '@angular/core/src/view';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
