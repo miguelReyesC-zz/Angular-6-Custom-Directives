139777a50f6da6af25dafc551662378b7b52caa2
Miguel <miguel_reyes_c@hotmail.com>
2018-09-13 23:04:02 -0300

    ngIf ngFor ngClass ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
